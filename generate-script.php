62915b83523786501bfaeb3c0e74d6e9f1ef0efb
Phantomical <phantom@lynches.ca>
2016-11-21 19:53:05 -0500

    Added script to compile code files

---
+++ generate-script.php
@@ -0,0 +1,128 @@
+<?php
+// A request to generate a code file
+class RequestValue {
+	// The language that the code is in
+	var $lang;
+	// The code text
+	var $text;
+};
+class RequestReturn {
+  // Compiler Output
+  var $output;
+  // Compilation Result
+  var $code;
+
+	void __construct($output, $code)
+	{
+		$this->output = $output;
+		$this->code = $code;
+	}
+};
+
+function generateRandomString($length = 10) {
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+// Compile a given java file with the file text
+// and a string indicating which java version to
+// compile it to
+//  text: The actual code text
+//  version_string: A string which is passed as an argument to
+//                  javac and used to indicate which version to
+//                  compile the code as
+function compileJavaFile($text, $version_string) {
+  $tmpdir = sys_get_temp_dir();
+  $output = "";
+  $result = "";
+  $filename = generateRandomString();
+
+  if(file_put_contents($tmpdir . $filename . ".java")
+  {
+    $output = shell_exec("javac -source " . $version_string . " " . $tmpdir . $filename . ".java");
+    $result = file_get_contents($tmpdir . $filename . ".class");
+
+    unlink($tmpdir . $filename . ".java");
+    unlink($tmpdir . $filename . ".class");
+  }
+	else
+	{
+		$output = "An unexpected error occurred while compiling the code.";
+	}
+
+	return new RequestReturn($output, $result);
+}
+
+// Compile a given file with the given source
+// code and extra arguments using Clang
+//  text: The C++ or C source code
+//  extargs: Extra args for the compiler
+function compileFileWithClang($extargs, $ext = ".cpp") {
+	$tmpdir = sys_get_temp_dir();
+	$output = "";
+	$result = "";
+	$filename = $tmpdir . generateRandomString();
+
+	if(file_put_contents($filename . $ext)) {
+  	$output .= shell_exec("./emcc -03 " . $filename . $ext . " -o " . $filename . ".bc" . $extargs);
+  	$output .= shell_exec("./emcc -03 " . $filename . ".bc -o " . $filename . ".js");
+
+		$result = file_get_contents($filename . ".js");
+
+  	unlink($filename . $ext);
+  	unlink($filename . ".bc");
+  	unlink($filename . ".js");
+	}
+	else {
+		$output .= "An unexpected error occurred while compiling the code.";
+	}
+
+	return new RequestReturn($output, )
+}
+
+//
+function compileUserFile($reqval)
+{
+	switch ($reqval->lang) {
+		case "C++14":
+			return compileFileWithClang($reqval->text, "-std=c++14");
+		case "C++11":
+			return compileFileWithClang($reqval->text, "-std=c++11");
+		case "C++98":
+			return compileFileWithClang($reqval->text, "");
+		case "C++1z": # NOTE: When C++1z is published this option must be changed
+			return compileFileWithClang($reqval->text, "-std=c++1z");
+		case "C89":
+			return compileFileWithClang($reqval->text, "-std=c89", ".c");
+		case "C99":
+			return compileFileWithClang($reqval->text, "-std=c99", ".c");
+		case "C11":
+			return compileFileWithClang($reqval->text, "-std=c11", ".c");
+		case "C94":
+			return compileFileWithClang($reqval->text, "-std=c94", ".c");
+		case "Java 1.3":
+			return compileJavaFile($reqval->text, "1.3");
+		case "Java 1.4":
+			return compileJavaFile($reqval->text, "1.4");
+		case "Java 1.5":
+			return compileJavaFile($reqval->text, "1.5");
+		case "Java 1.6":
+			return compileJavaFile($reqval->text, "1.6");
+		case "Java 1.7":
+			return compileJavaFile($reqval->text, "1.7");
+		case "Objective-C++":
+			return compileFileWithClang($reqval->text, "", ".mm");
+		case "Objective-C":
+			return compileFileWithClang($reqval->text, "", ".m");
+		default:
+			# An unknown language was passed
+			# this is an error
+			break;
+	}
+}
+?>
