aa74f5eb11cad2edbcbe58f0c27995f4b45d68d0
Phantomical <phantom@lynches.ca>
2016-12-04 16:34:59 -0500

    Moved tests.py implementation into repository

--- convert-to-js.py
+++ convert-to-js.py
@@ -20,10 +20,10 @@ class RequestValue:
        self.text = text
        
 def writeFile(filename, text):
-    with open(filename, "w") as f:
+    with open(filename, "wb") as f:
         f.write(text)
 def readFile(filename):
-    with open(filename, "r") as f:
+    with open(filename, "rb") as f:
         return f.read()
 
 def mkTempDir():
@@ -52,126 +52,111 @@ def minifyJS(jsfile, level = 'ADVANCED_OPTIMIZATIONS'):
 #                  javac and used to indicate which version of
 #                  java the source file is
 def compileJava(text, version_string):
-    result = ''
-
-    # Java compilation is not implemented yet
-    raise NotImplementedError("Java compilation has not been implemented yet");
+    raise NotImplementedError("Compiling Java is not supported at this time.")
 
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
+        
+        writeFile(filename + '.java', text)
 
-    writeFile(filename + '.java', text)
+        exec_command('javac -source ' + version_string + ' ' + filename + '.java')
 
-    exec_command('javac -source ' + version_string + ' ' + filename + '.java')
+        for file in fnmatch.filter(os.listdir(tmpdir), '*.class'):
+            continue # TODO: Make this do something
+        
+        return readFile(file)
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    for file in fnmatch.filter(os.listdir(tmpdir), '*.class'):
-        continue # TODO: Make this do something
-    
-    result = readFile(file)
-
-    shutil.rmtree(tmpdir, True)
-
-    return result
 # Compile a given file with the given source
 # code and extra arguments using Clang
 #  text: The C++ or C source code
 #  extargs: Extra args for the compiler
 #  ext: The file extension that the generated file will have
 def compileWithClang(text, extargs = '', ext = '.cpp'):
-    result = ''
-
     tmpdir = mkTempDir()
-    filename = tmpdir + '\\tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
 
-    writeFile(filename + ext, text)
-    filename = tmpdir + 'tmpfile'
+        writeFile(filename + ext, text)
+        filename = tmpdir + 'tmpfile'
 
-    exec_command('emcc -O3 -Wall ' + filename + ext + ' -o ' + filename + '.bc ' + extargs)
-    exec_command('emcc -O3 ' + filename + '.bc -o ' + filename + '.js')
-    minifyJS(filename + '.js')
+        exec_command('emcc -O3 -Wall ' + filename + ext + ' -o ' + filename + '.bc ' + extargs)
+        exec_command('emcc -O3 ' + filename + '.bc -o ' + filename + '.js')
+        minifyJS(filename + '.js')
 
-    result = readFile(filename + '.js')
+        return readFile(filename + '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    shutil.rmtree(tmpdir, True)
-
-    return result
 def compileHaskell(text):
-    result = None
-
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
+        
+        writeFile(filename + '.hs', text)
+        
+        exec_command('ghcjs ' + filename + '.hs -o ' + filename + '.js')
+        minifyJS(filename + '.js')
+        
+        return readFile(filename + '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    writeFile(filename + '.hs', text)
-
-    exec_command('ghcjs ' + filename + '.hs -o ' + filename + '.js')
-    minifyJS(filename + '.js')
-
-    result = readFile(filename + '.js')
-
-    shutil.rmtree(tmpdir, True)
-
-    return result
 def compileTypeScript(text):
-    result = None
-
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
 
-    writeFile(filename + '.ts', text)
+        writeFile(filename + '.ts', text)
 
-    exec_process('tsc ' + filename + '.ts')
-    
-    result = readFile(filename + '.js')
+        exec_process('tsc ' + filename + '.ts')
+        
+        return readFile(filename + '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    shutil.rmtree(tmpdir, True)
-
-    return result
 def compileCoffeeScript(text):
-    result = None
-
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
 
-    writeFile(filename + '.coffee', text)
+        writeFile(filename + '.coffee', text)
 
-    exec_command('coffee -o ' + tmpdir + ' -c ' + filename)
+        exec_command('coffee -o ' + tmpdir + ' -c ' + filename)
 
-    result = readFile(filename + '.js')
+        return readFile(filename + '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    shutil.rmtree(tmpdir, True)
-
-    return result
 def compilePython3(text):
-    result = None
-
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
 
-    writeFile(filename + '.js', text)
+        writeFile(filename + '.js', text)
 
-    exec_command('pyjsbuild ' + fliename + '.py -o ' + filename + '.js')
+        exec_command('pyjsbuild ' + fliename + '.py -o ' + filename + '.js')
 
-    result = readFile(filename + '.js')
+        return readFile(filename + '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
-    shutil.rmtree(tmpdir, True)
-
-    return RequestReturn(output, result)
 def compileCOBOL(text, dialect):
-    result = None
     tmpdir = mkTempDir()
-    filename = tmpdir + 'tmpfile'
+    try:
+        filename = tmpdir + 'tmpfile'
 
-    writeFile(filename + '.cbl', text)
+        writeFile(filename + '.cbl', text)
 
-    exec_command('cobc -C -Wall ' + filename + '.cbl -o ' + filename + '.c')
-    exec_command('emcc -O3 ' + filename + '.c -o ' + filename + '.bc')
-    exec_command('emcc -O3 ' + filename + '.bc -o ' + filename + '.js')
-    
-    result = readFile(filename = '.js')
-    
-    shutil.rmtree(tmpdir, True)
-
-    return result
+        exec_command('cobc -C -Wall ' + filename + '.cbl -o ' + filename + '.c')
+        exec_command('emcc -O3 ' + filename + '.c -o ' + filename + '.bc')
+        exec_command('emcc -O3 ' + filename + '.bc -o ' + filename + '.js')
+        
+        return readFile(filename = '.js')
+    finally:
+        shutil.rmtree(tmpdir, True)
 
 def compileUserFile(lang, code):
      default = lambda text: ''
@@ -189,6 +174,7 @@ def compileUserFile(lang, code):
          'Java 1.5':      lambda text: compileJava(text, '1.5'),
          'Java 1.6':      lambda text: compileJava(text, '1.6'),
          'Java 1.7':      lambda text: compileJava(text, '1.7'),
+         'Java 1.8':      lambda text: compileJava(text, '1.8'),
          'Objective-C++': lambda text: compileWithClang(text, '', '.mm'),
          'Objective-C'  : lambda text: compileWithClang(text, '', '.m'),
          'Haskell':       lambda text: compileHaskell(text),
@@ -201,9 +187,7 @@ if __name__ == '__main__': # Script was executed from the command line
     if len(sys.argv) < 3:
         print('usage: convert-to-js <input-file> <output-file>')
     else:
-        text = readFile(sys.argv[1])
-        input = json.loads(text)
-        output = compileUserFile(input['lang'], input['text'])
+        input = json.load(open(sys.argv[1]))
+        output = compileUserFile(input.lang, input.text)
         writeFile(sys.argv[2], output)
-        sys.exit()
         
---
+++ tests.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+from colorama import init, Fore
+from subprocess import Popen, check_output
+import subprocess
+import json
+import os
+
+init(autoreset=True)
+
+class test:
+    def __init__(self, name, lang, code):
+        self.json = json.dumps({ 'text': code, 'lang': lang })
+        self.name = name
+
+hello_world_tests = [ # All of these should end up printing 'Hello, World!' when run
+    test('C++14 Hello World', 'C++14', '#include <iostream>\nint main(int argc, char** argv) { std::cout << "Hello, World!" << std::endl; }'),
+    test('C++11 Hello World', 'C++11', '#include <iostream>\nint main(int argc, char** argv) { std::cout << "Hello, World!" << std::endl; }'),
+    test('C++03 Hello World', 'C++03', '#include <iostream>\nint main(int argc, char** argv) { std::cout << "Hello, World!" << std::endl; }'),
+    test('C++98 Hello World', 'C++98', '#include <iostream>\nint main(int argc, char** argv) { std::cout << "Hello, World!" << std::endl; }'),
+    test('C++1z Hello World', 'C++1z', '#include <iostream>\nint main(int argc, char** argv) { std::cout << "Hello, World!" << std::endl; }'),
+    test('C11 Hello World', 'C11', '#include <stdio.h>\nint main(int argc, char** argv) { printf("Hello, World!"); }'),
+    test('C99 Hello World', 'C99', '#include <stdio.h>\nint main(int argc, char** argv) { printf("Hello, World!"); }'),
+    test('C89 Hello World', 'C89' '#include <stdio.h>\nint main(int argc, char** argv) { printf("Hello, World!"); }'),
+    test('C94 Hello World', 'C94' '#include <stdio.h>\nint main(int argc, char** argv) { printf("Hello, World!"); }'),
+    test('COBOL Hello World', 'COBOL', 
+         """
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLOWORLD.
+
+PROCEDURE DIVISION.
+    DISPLAY 'Hello, World!'.
+    STOP RUN.
+         """),
+    test('Java 1.3 Hello World', 'Java 1.3', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Java 1.4 Hello World', 'Java 1.4', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Java 1.5 Hello World', 'Java 1.5', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Java 1.6 Hello World', 'Java 1.6', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Java 1.7 Hello World', 'Java 1.7', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Java 1.8 Hello World', 'Java 1.8', 'class HelloWorld{ public static void main(String[]args){ System.out.println("Hello, World!"); } }'),
+    test('Haskell', 'Haskell', 'main = putStrLn "Hello, World!"'),
+]
+
+class TestFailedException:
+    def __init__(self):
+        return
+
+def run_test(testval, expected):
+    try:
+        with open('testfile.in', 'w') as f:
+            f.write(testval.json)
+
+        print (Fore.CYAN + "[Running test '" + testval.name + "']")
+
+        subprocess.run(['./convert-to-js.py', 'testfile.in', 'testfile.js'], shell=True, check=True)
+
+        os.unlink('testfile.in')
+
+        output = check_output('node testfile.js')
+        
+        os.unlink('testfile.js')
+
+        if output != expected:
+            raise TestFailedException();
+
+        print (Fore.GREEN + "[Test Succeeded]")
+        return True
+    except:
+        print (Fore.RED + "[Test Failed]")
+        pass
+    return False
+
+if __name__ == '__main__':
+    result = True
+
+    print (Fore.CYAN + "[Running Tests]")
+
+    for tst in hello_world_tests:
+        val = run_test(tst, "Hello, World")
+        result = result and val
+
+    if not result:
+        print (Fore.RED + "[Tests Failed]")
+        raise TestFailedException()
